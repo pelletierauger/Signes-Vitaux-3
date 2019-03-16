85b1ad2d7128a66d7df649ffbf923c1eff35f603
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2019-03-16 18:30:14 -0400

    Establish a solid ambient base

--- send-supercollider.js
+++ send-supercollider.js
@@ -5,26 +5,36 @@ sendSuperCollider = function() {
         if ((intensity > 0) !== positive) {
             positive = (intensity > 0);
             console.log("Changed!");
-            let har = (positive) ? 0 : -2;
+            let har = (positive) ? 0.257 : 0.584;
+            let key = (positive) ? "Db1" : "D#1";
 //             socket.emit('supercollider', {
 //                 name: "/har",
 //                 value: har
 //             });
+            logJavaScriptConsole(har);
             socket.emit('sendOSC', {
                 address: "/har",
                 type: "f",
                 value: har
+            });
+            socket.emit('sendOSC', {
+                address: "/key",
+                type: "s",
+                value: key
             });
         }
         // console.log(intensity);
         intensity = Math.abs(intensity);
         intensity = map(intensity, 0.007, 1, 0.005, 0.015);
         intensity = constrain(intensity, 0.005, 0.015);
-        let loc = Math.floor(map(intensity, 0.005, 0.015, 400000, 350000));
+//         logJavaScriptConsole(intensity); 
+//         let loc = Math.floor(map(intensity, 0.005, 0.015, 0, 0.004));
+        let loc = map(intensity, 0.005, 0.015, 0.004, 0) ;
 //         socket.emit('supercollider', {
 //             name: "/loc",
 //             value: loc
 //         });
+//         logJavaScriptConsole(loc);
         socket.emit('sendOSC', {
             address: "/loc",
             type: "f",
@@ -43,5 +53,5 @@ sendSuperCollider = function() {
     }
 }
 
-// drawIncrement = 0.01;
+drawIncrement = 0.00125;
 // log("ms");
