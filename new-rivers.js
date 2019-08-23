5766031547e88c91d988d939585b41e06111bdb4
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2019-08-23 05:00:15 -0400

    Work on new-rivers.js

--- new-rivers.js
+++ new-rivers.js
@@ -177,13 +177,14 @@ void main() {
     float d = length(uv);
     float t = time * 0.125 * 0.06125;
     uv.y += -0.25; 
-    uv.x += -0.25; 
-    uv *= 2.5;
+    uv.x += -0.5; 
+    uv *= 5.5;
     for (int i = 0; i < 10; i += 1) {
       float s = float(i) + 1.0;
       float e = circ(20.5 * s, 2.0 - (s * -0.15), uv.x, uv.y, 1.0 - (s * 0.025));
       c = c + e;
     }
+//     uv *= 0.5;
     c = sin(c * 0.5) + 1.0 * 2.0;
     float c2 = cos(c * 0.95) + 1.0 * 1.;
     vec2 p = vec2(1.0, -1.4);
@@ -196,13 +197,116 @@ void main() {
     col *= sin(sin(atan(newV.x, newV.y) * (sin(t * 100.) * 1.)) * 10. + sin(t * 10.) * 100.);
     col *= sin(cos(atan(newV.x, newV.y) * (sin(t * 100.) * 1.)) * 10. + cos(t * 10.) * 100.);
     col *= sin(sin(atan(newV.x, newV.y) * (sin(t * 100.) * 1.)) * 10. + cos(t * 10.) * 100.);
-    col += CircleRGB(uv, p2, 0.4, 0.2, vec3(0.0, 0.4, 1.0));
-    col += CircleRGB(uv, p2, 0.5, 0.2, vec3(0.5, 0.7, 0.0) * 0.75);
+//     col += CircleRGB(uv, p2, 0.4, 0.2, vec3(0.0, 0.4, 1.0));
+//     col -= CircleRGB(uv, p2, 0.5, 0.2, vec3(1.5, -0.7, 0.0) * 0.75);
     col.g *= 0.5;
     float rando = rand(uv);
     float pix = 1.0 - c * 0.045 + (rando * 0.1);
     vec3 finCol = vec3(1.0 - c2 * 1.0 * loww, 1.5 - pix * 1.2, (1. - c2 * 0.5) * 1.);
-    finCol -= col * 1.85;
+    finCol -= col * 0.95;
     gl_FragColor = vec4(finCol, 1.0);
 }
 `);
+
+
+
+
+
+
+drawIncrement *= 2;
+
+drawCount = 20.5;
+logJavaScriptConsole(drawIncrement);
+drawIncrement = 0.000125;
+// drawIncrement *= 2;
+// --------------- //
+// vagues simples- //
+// ------ --------- //
+setBothShaders(`
+precision mediump float;
+varying vec2 vTexCoord;
+uniform float time;
+float plot(vec2 s, float p) {
+  float largeur = abs(sin(time * 0.01)) * 0.1 + 0.1;
+  return smoothstep(p - largeur, p, s.y) - smoothstep(p, p + largeur, s.y);
+}
+float circ(float size, float vx, float vy, vec2 center) {
+//   float x = center.x * (1.0 + sin(time * 20.) * 0.5);
+//   float y = center.y * (1.0 + cos(time * 20.) * 0.5);
+  vec2 v = center - vec2(vx, vy);
+  float d = 1.0 / length(v * size);
+  return d;
+}
+float Circle(vec2 uv, vec2 p, float r, float blur) {
+    float d = length(uv - p); 
+    float c = smoothstep(r, r - blur, d); 
+    return c;
+}
+vec3 CircleRGB(vec2 uv, vec2 p, float r, float blur, vec3 col) {
+    float d = length(uv - p); 
+    float c = smoothstep(r, r - blur, d); 
+    return col * c;
+}
+vec3 InvCircleRGB(vec2 uv, vec2 p, float r, float blur, vec3 col) {
+    float d = length(p - uv); 
+    float c = smoothstep(r - blur, r, d); 
+    return col * c;
+}
+float rand(vec2 co){
+    return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453 * (2.0 + sin(time)));
+}
+void main() {
+    vec2 uv = gl_FragCoord.xy / vec2(1280, 800);
+    uv -= vec2(0.5, 0.5);
+    uv.x *= 1280.0/800.0;
+//      uv *= 1.2;
+    vec2 uvf = uv * 10.;
+//     uv.x += 0.25;
+//     uv.y += 0.4;
+//     uv *= 0.5;
+    float d = length(uv);
+    float t = time * 0.125 * 0.06125 * 0.5;
+    t *= 2.;
+//     t = time * 0.125;
+    float c = d;
+//     if (d < 0.3) c = 1.; else c = 0.;
+    float r = 0.3;
+//     c = smoothstep(r, r - 0.1, d);
+//     c = c - smoothstep(r * 0.5, r * 0.5 - 0.1, d);
+    vec2 p = vec2(1.0, -1.4);
+    c = Circle(uv, p, 0.3, 0.1);
+    vec2 p2 = vec2(0.1 * sin(t * 100.), 0.1 * cos(t * 100.)) * 0.4;
+    vec2 p3 = vec2(0.1 * sin(t * 50.), 0.1 * cos(t * 50.)) * 0.4;
+    float siz = sin(t * 100.);
+    float siz2 = sin(t * 50.);
+    p2 = vec2(0.0, 0.0);
+    vec3 col = CircleRGB(uv, p2, 1.9, 0.2, vec3(1., 1.4, 1.0));
+    col.r *= sin(length(uv - p) * (6. + sin(1. * 20.) * 0.0001));
+    col.r += abs(sin(length(uv - p) * (0.1 + sin(1. * 2.) * 0.001)) * 5.);
+//     col.g *= sin(length(uv - p) * (6. + sin(1. * 20.) * 0.01));
+    vec2 newV = (uv + tan(uv.y * 3.)) - (p + sin(uv.x * 3.));
+    newV += cos(sin(uv * 20.));
+//     
+//     lignes contours
+//     newV.x *= cos(uv.y * sin(t * 100.) * 100.) * 100.;
+//     
+//     col *= sin(atan(newV.x, newV.y));
+      col *= sin(sin(atan(newV.x, newV.y) * (sin(t * 100.) * 1.)) * 10. + sin(t * 10.) * 100.);
+      col *= sin(cos(atan(newV.x, newV.y) * (sin(t * 100.) * 1.)) * 10. + cos(t * 10.) * 100.);
+      col *= sin(sin(atan(newV.x, newV.y) * (sin(t * 100.) * 1.)) * 10. + cos(t * 10.) * 100.);
+//col -=  CircleRGB(uv, p2, 0.2, 0.2, vec3(1.0, 1.0, 1.0));
+//     col = max(col, 0.);
+//col += CircleRGB(uv, p2, 0.5, 0.1, vec3(0.2, 0.0, 0.8));
+    col += CircleRGB(uv, p2, 0.4, 0.2, vec3(0.0, 0.4, 1.0));
+            col += CircleRGB(uv, p2, 0.5, 0.2, vec3(0.5, 0.7, 0.0) * 0.75);
+    col += CircleRGB(uv, p2, 1.5, 0.2, vec3(0.75, 0.2, 0.0) * 0.75);
+//             col += CircleRGB(uv, p2, 0.5, 0.2, vec3(0.5, 0.7, 0.0));
+//     col -= InvCircleRGB(uv, p2, 0.6, 0.2, vec3(1.5, 1.7, 1.0));
+//     col.g = min(1.5, col.g);
+    float rando = rand(uvf) * 0.1;
+//     col.r -= rando * 1.;
+     col.g *= 0.5;
+    gl_FragColor = vec4(col - rando, 1.0);
+}
+`);
+
